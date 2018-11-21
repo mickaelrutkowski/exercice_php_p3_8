f08cdeb1490527e15ff41f1ea2f571616405652e
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-21 09:32:55 +0100

    exercice_php_P3_8

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  $num = 200;
+  while ($num > 0){
+    echo ' ' . ' Enfin !!!!';
+   $num-= 12;
+  }
+ ?>
+</body>
+</html>
